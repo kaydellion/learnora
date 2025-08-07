2b2c64c964e8bfdede9cea844b7c6c5b32729005
fopsycute <fopsycute18@gmail.com>
2025-08-07 07:32:54 +0100

    slug change

--- type-card.php
+++ type-card.php
@@ -5,7 +5,7 @@
           <div class="promo-content">
                 <p class="small-text"><?php echo $category_name; ?></p>
 
-                <a href="<?php echo $siteurl; ?>event-type?event-slug=<?php echo $slugs; ?>" class="btn-shop">Shop Now</a>
+                <a href="<?php echo $siteurl; ?>event-type/<?php echo $slugs; ?>" class="btn-shop">Shop Now</a>
               </div>
               </div>
                 </div>
