a034a56234007ee6d22fa3cfa639fa830a30191d
fopsycute <fopsycute18@gmail.com>
2025-08-09 13:39:39 +0100

    test

--- models.php
+++ models.php
@@ -25,7 +25,7 @@ table th {
 }
 </style>
 <div class="container-xxl mt-5 mb-5">
-<p><a href="add-model.php" class="btn btn-primary">Add Models</a> </p>
+<p><a href="add-training.php" class="btn btn-primary">Add Training</a> </p>
               <!-- Hoverable Table rows -->
                 <h5 class="card-header"> Manage Resources </h5>
                 <div class="table-responsive text-nowrap">
@@ -33,74 +33,65 @@ table th {
                   <thead>
                   <tr>
                   <th>S/N</th>
-                  <th>Image</th>
-                  <th>Report Title</th>
+             
+                  <th> Title</th>
                   <th>Category</th>
                   <th>Subcategory</th>
                   <th>Pricing</th>
                   <th>Price</th>
                   <th>Tags</th>
-                  <th>Loyalty</th>
-                  <th>Uploaded by</th>
                   <th>Created Date</th>
-                  <th>Last Updated</th>
                   <th>Status</th>
                   <th>Actions</th>
                   </tr>
                   </thead>
                   <tbody class="table-border-bottom-0">
                 <?php 
-                $query = "SELECT r.*,u.display_name, l.category_name AS category,ri.picture, sc.category_name AS subcategory 
-                  FROM ".$siteprefix."reports r 
-                  LEFT JOIN ".$siteprefix."categories l ON r.category = l.id 
-                  LEFT JOIN ".$siteprefix."users u ON r.user = u.s 
-                   LEFT JOIN ".$siteprefix."reports_images ri ON r.id = ri.report_id
-                  LEFT JOIN ".$siteprefix."categories sc ON r.subcategory = sc.id WHERE user = '$user_id' GROUP BY r.id";
+                     $query = "SELECT t.*, u.name as display_name,lu.display_name AS nameupload, tt.price, u.photo as profile_picture, l.category_name AS category, sc.category_name AS subcategory
+    FROM ".$siteprefix."training t
+    LEFT JOIN ".$siteprefix."categories l ON t.category = l.id 
+    LEFT JOIN ".$siteprefix."instructors u ON t.instructors = u.s
+    LEFT JOIN ".$siteprefix."users lu ON t.user = lu.s
+    LEFT JOIN ".$siteprefix."categories sc ON t.subcategory = sc.id 
+    LEFT JOIN ".$siteprefix."training_tickets tt ON t.training_id= tt.training_id
+   Where t.status='approved' AND t.user = '$user_id' GROUP BY t.training_id";
                 $result = mysqli_query($con, $query);
                 if (!$result) {
                     die('Query Failed: ' . mysqli_error($con));
                 }
                 $result = mysqli_query($con, $query);
                 if(mysqli_num_rows($result) == 0) {
-                  echo '<tr><td colspan="14" class="text-center">No reports found. <a href="add-model.php">Add New</a></td></tr>';
+                  echo '<tr><td colspan="14" class="text-center">No reports found. <a href="add-training.php">Add New</a></td></tr>';
                 } else{
                 $i = 1;
                 while($row = mysqli_fetch_assoc($result)) {
-                $report_id = $row['id'];
-                $report_row = $row['s'];
-                $title = $row['title'];
-                $description = $row['description'];
-                $category = $row['category'];
-                $subcategory = $row['subcategory'];
-                $pricing = $row['pricing'];
-                $price = $row['price'];
-                $tags = $row['tags'];
-                $loyalty = $row['loyalty'];
-                $loyalty_class = ($loyalty == 0) ? 'bg-danger' : 'bg-success';
-                $loyalty = "<span class='badge $loyalty_class'>-</span>";
-                $user = $row['display_name'];
-                $created_date = $row['created_date'];
-                $updated_date = $row['updated_date'];
-                $image_path = $imagePath.$row['picture'];
-                $status = $row['status'];
+                $training_id = $row['training_id'];
+        $title = $row['title'];
+        $alt_title = $row['alt_title'];
+        $description = $row['description'];
+        $category = $row['category'];
+        $subcategory = $row['subcategory'];
+        $pricing = $row['pricing'];
+        $price = $row['price'];
+        $tags = $row['tags'];
+        $uploaded_by = $row['nameupload'];
+        $user = $row['display_name'];
+        $user_picture = $imagePath.$row['profile_picture'];
+        $created_date = $row['created_at'];
+        $status = $row['status'];
                 ?>
                   <tr>
                   <td><strong><?php echo $i; ?></strong></td>
-                  <td><img src="<?php echo htmlspecialchars($image_path); ?>"  alt="<?php echo htmlspecialchars($image_path); ?>" class="img-fluid img-small rounded"></td>
-                  <td><?php echo $title; ?></td>
                   <td><?php echo $category; ?></td>
                   <td><?php echo $subcategory; ?></td>
                   <td><?php echo $pricing; ?></td>
-                  <td><?php echo $price; ?></td>
                   <td><?php echo $tags; ?></td>
-                  <td class="text-center"><?php echo $loyalty; ?></td>
-                  <td><?php echo $user; ?></td>
                   <td><?php echo $created_date; ?></td>
-                  <td><?php echo $updated_date; ?></td>
+
                   <td><span class="badge bg-<?php echo getBadgeColor($status); ?> me-1"><?php echo $status; ?></span></td>
                   <td>
-                   <a href="edit-report.php?report=<?php echo $report_id; ?>" class="btn btn-sm btn-warning me-1"><i class="bx bx-edit-alt me-1"></i> Edit Report</a>
-                  <a href="delete.php?action=delete&table=reports&item=<?php echo $report_row; ?>&page=<?php echo $current_page; ?>" class="btn btn-sm btn-danger delete"><i class="bx bx-trash me-1"></i> Delete </a></td>
+                   <a href="edit-training.php?training=<?php echo $training_id; ?>" class="btn btn-sm btn-warning me-1"><i class="bx bx-edit-alt me-1"></i> Edit Report</a>
+                  <a  href="delete.php?action=delete&table=training&item=<?php echo $training_id; ?>&page=<?php echo $current_page; ?>" class="btn btn-sm btn-danger delete"><i class="bx bx-trash me-1"></i> Delete </a></td>
                   </tr>
                   <?php $i++; }} ?>
                   </tbody>
@@ -108,8 +99,6 @@ table th {
                 </div>
                 </div>
               <!--/ Hoverable Table rows -->
-
-            
 
             </div>
 
