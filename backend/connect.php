a46b970da0da8a147948c3724362884520eb0e4b
fopsycute <fopsycute18@gmail.com>
2025-08-12 04:24:18 +0100

    added database

--- backend/connect.php
+++ backend/connect.php
@@ -7,15 +7,9 @@ error_reporting(E_ALL); ini_set('display_errors', 1); ini_set('log_errors', 1);
 $db_host = "localhost"; 
 
 /*
-
-
-
 $db_username = "root"; 
 $db_pass = ""; 
 $db_name = "learnora";
-
-
-
 */
 
 
