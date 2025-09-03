37c48b6bf4aaabd859f3ee9d855ca879b066ec02
fopsycute <fopsycute18@gmail.com>
2025-09-03 07:09:42 +0100

    change class name

--- blog-card.php
+++ blog-card.php
@@ -3,7 +3,7 @@
     <a href="<?php echo $siteurl; ?>view-blog/<?php echo $alt_title; ?>">
   <article class="post-item side-post">
     <div class="post-img">
-  <img src="<?php echo $siteurl.$image_path; ?>" alt="" class="img-fluid">
+  <img src="<?php echo $siteurl.$image_path; ?>" alt="" class="img-fluid post-author-image">
     <div class="category-container">
 <?php foreach ($catNames as $cat): ?>
         <span class="category"><?php echo htmlspecialchars($cat); ?></span>
@@ -13,7 +13,7 @@
       <a href="<?php echo $siteurl; ?>view-blog/<?php echo $alt_title; ?>"><?php echo $title; ?></a>
     </h2>
     <div class="d-flex align-items-center">
-      <img src="<?php echo $siteurl;?>/uploads/<?php echo $siteimg; ?>" alt="site image" class="img-fluid post-author-image flex-shrink-0">
+      <img src="<?php echo $siteurl;?>/uploads/<?php echo $siteimg; ?>" alt="site image" class="img-fluid post-author-img flex-shrink-0">
       <div class="post-meta">
         <p class="post-author"><?php echo $sitename; ?></p>
         <p class="post-date">
